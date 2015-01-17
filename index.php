192e3b521b226f60aba2c0380308ababc81cfc9b
meb4byn <meb4byn@gmail.com>
2015-01-17 14:17:41 -0700

    created new structure for modularizing code
    must install xampp to run and checkout into xamp/htdocs

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="High Desert Off-Road Club is...">
+  <meta name="keywords" content="off-road,truck,buggy,jeep,rock,mud">
+  <title>High Desert Off-Road</title>
+  <link rel="icon" type="image/x-icon" href="../media/favicon.ico">
+  <link rel="shortcut icon" href="../media/favicon.ico">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+  <?php include 'php/nav.php';?>
+  
+  <div class="container"> <!-- container -->
+    <div class="row"> <!-- Logo -->
+      <div class="col-sm-4">
+        <a class="brand" href="#">
+          <img class="img-responsive" src="img/lizard.png"></img>
+        </a>
+      </div>
+      <div class="col-sm-8">
+      </div>
+    </div> <!-- end logo -->
+
+    <div class="row"> <!-- Main Content Start -->
+
+    </div> <!-- Main Content end -->
+
+    <?php include 'php/footer.php';?>
+  </div> <!-- end container -->
+
+<!-- javascript files -->
+<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ php/nav.php
@@ -0,0 +1,21 @@
+<div class="row">
+  <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="home.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="friends.html">Friends</a></li>
+          <li><a href="events.html">Events</a></li>
+          <li><a href="contact.html">Contact Us</a></li>  
+        </ul> 
+    </div>
+  </nav> 
+</div>
