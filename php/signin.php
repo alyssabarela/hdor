20d485ebfffe4c8dbf8d891ef13ba55bc819295c
meb4byn <meb4byn@gmail.com>
2015-01-21 09:22:30 -0700

    Create signin.php

---
+++ php/signin.php
@@ -0,0 +1,23 @@
+<div class="container">
+  <img src="img/logo.png" alt="logo"/>
+  <h2 class="logintitle">Title</h2>
+  <div class="row">
+    <div class="col-sm-6 col-sm-offset-3">
+      <div class="modal-body">
+        <form class="form col-sm-12 center-block">
+          <div class="form-group">
+            <input type="text" class="form-control input-lg" placeholder="Email">
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control input-lg" placeholder="Password">
+          </div>
+          <div class="form-group">
+            <button onclick="login()" class="btn btn-default btn-lg btn-block signin">Sign In</button>
+            <a href="landingpage.cfm">Enter</a>
+          </div>
+        </form>
+        <div class="clearfix"></div>
+      </div>
+    </div>
+  </div>
+</div>
