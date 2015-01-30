230e3dbde9aac238fe86f5edcc8c6422265852c4
meb4byn <meb4byn@gmail.com>
2015-01-30 16:47:16 -0700

    add carousel

--- index.php
+++ index.php
@@ -3,17 +3,14 @@
 <!-- row2: header -->
 <?php include 'php/header.php';?>
 
-<div class="row">
-    <?php include 'php/carousel.php';?>
-</div>
-
 <!-- row 3: article/aside -->
 <div class="row"> <!-- Main Content Start -->
   <div class="col-sm-3 col-sm-push-9">
-    <?php include 'php/side_column_item.php';?>
+    <?php include 'php/upcomingEvents.php';?>
   </div>
   <div class="col-sm-9 col-sm-pull-3">
-    <?php include 'php/page_body_item.php';?>
+    <?php include 'php/carousel.php';?>
+    <?php include 'php/welcome.php';?>
   </div>
 </div> <!-- Main Content end -->
 
---
+++ php/upcomingEvents.php
@@ -0,0 +1,10 @@
+<!-- side column item -->
+<aside>
+  <h2>Upcoming Events</h2>
+  <ul>
+    <li>Event 1</li>
+    <li>Event 2</li>
+    <li>Event 3</li>
+    <li>Event 4</li>
+  </ul>
+</aside>
