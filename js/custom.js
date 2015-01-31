ba05cc01838950a9da60fc3eda505fe3faf13dd6
meb4byn <meb4byn@gmail.com>
2015-01-31 15:28:43 -0700

    clean up custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,6 @@
 /** custom js **/
 
 // highlight current navbar tab
-// $('.navbar li a').click(function(){
-//   $(this).closest('li').addClass('active').siblings().removeClass('active');
-//   return(false);
-// });
-// other option need to add id 'home' to body tag
 $('#Home a:contains("Home")').parent().addClass('active');
 $('#About a:contains("About")').parent().addClass('active');
 $('#Friends a:contains("Friends")').parent().addClass('active');
